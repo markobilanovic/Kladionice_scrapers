7d20d88adb8099570d82266a8ce4fac8b1085e35
Marko Bilanovic <bilanovic90@gmail.com>
2017-06-15 20:02:30 +0200

    maxbet update

--- maxbet.js
+++ maxbet.js
@@ -24,7 +24,7 @@ var casper = require("casper").create({
     loadPlugins: false,
     userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.94 Safari/537.4'
   },
-  clientScripts: []
+  clientScripts: ["vendor/jquery.min.js", "vendor/lodash.js"]
 });
 
 
@@ -37,114 +37,20 @@ var visitor = [];
 var zeroTwo = [];
 var threePlus = [];
 
-var divNumber;
-var oldDivNumber = 0;
+var currentLeague = 3;
+var leaguesCount = 0;
 
 
-function outputJSON(){
-	output.push({
-		home: home,
-		visitor: visitor
-	});
-	return JSON.stringify(outputJSON);
+function getNextLeagueDivSelector() {
+  var divSelector = "//*[@id=\"topView\"]/div[1]/div[2]/div[3]/div/div[1]/div[2]/div[" + currentLeague + "]/p";
+  return divSelector;
 }
 
-function getHome() {
-  //without jQuery
-  var matchesDiv = document.querySelectorAll('div.matches-info-table-holder');
-  var home = matchesDiv[matchesDiv.length-1].
-      querySelectorAll('div.home-visitor-holder > div.home');
-  //var home = document.querySelectorAll('div.navigators-holder div:nth-child(2) div.home-visitor-holder > div.home');
-  return Array.prototype.map.call(home, function(e) {
-    return e.innerText;
-  });
-
-  //with jQuery
-  /*var home = $('.home');
-  return _.map(home, function(e){
-    return e.innerText;
-  });
-  */
-}
-
-function getVisitor() {
-  var matchesDiv = document.querySelectorAll('div.matches-info-table-holder');
-  var visitor = matchesDiv[matchesDiv.length-1].
-      querySelectorAll('div.home-visitor-holder > div.visitor');
-  return Array.prototype.map.call(visitor, function(e) {
-    return e.innerText;
-  });
-}
-
-function getZeroTwo() {
-  var matchesDiv = document.querySelectorAll('div.matches-table-holder');
-  var zeroTwoDiv = matchesDiv[matchesDiv.length-1].querySelectorAll('.one-match-row');
-  var el;
-  var zeroTwoA = [];
-
-  for(var i = 0; i < zeroTwoDiv.length; i++) {
-    el = zeroTwoDiv[i].querySelector('div:nth-child(6)');
-    zeroTwoA.push(el.innerText);
-  }
-
-  return zeroTwoA;
-}
-
-function getThreePlus() {
-  var matchesDiv = document.querySelectorAll('div.matches-table-holder');
-  var threePlusDiv = matchesDiv[matchesDiv.length-1].querySelectorAll('.one-match-row');
-  var el;
-  var threePlus = [];
-
-  for(var i = 0; i < threePlusDiv.length; i++) {
-    el = threePlusDiv[i].querySelector('div:nth-child(7)');
-    threePlus.push(el.innerText);
-  }
-
-  return threePlus;
-}
-
-function forseTerminate(){
-  this.echo('thats all folks. ').exit();
-}
-
-function saveAndExit() {
-  var csvData = '';
-  for(var i = 0; i < home.length; i++){
-    csvData += home[i] + ", " + visitor[i] + ', ' + zeroTwo[i] + ', ' + threePlus[i] + "\n";
-    this.echo(home[i] + ' - ' + visitor[i] + ' : ' + zeroTwo[i] + ':' + threePlus[i]);
-  }
-
-  fs.write(exportFilename, csvData, 'w');
-  this.echo('DONE!').exit();
-};
-
-function getTableCount() {
-	var matchesDiv = document.querySelectorAll('div.league-wrapper.ng-scope');
-	return matchesDiv.length;
-}
-
-
-var scrollDown = function() {
-	oldDivNumber = divNumber;
-	casper.echo('Usao');
-	casper.scrollToBottom();
-	casper.wait(5000, function() {
-		console.log('wait scrollToBottom...');
-	});
-	
-	casper.echo('call getTableCount()');
-	divNumber = document.querySelectorAll('div.league-wrapper.ng-scope').length;
-	casper.echo("divNumber = " + divNumber);
-	
-	casper.waitFor(function() { return oldDivNumber != divNumber}, scrollDown, forseTerminate);
-};
-
-
-
-
-function wait(){
-	this.wait(2000, function(){ this.echo("Waiting after scrolling..."); })	
+function getLeaguesCount() {
+  var fudbalDiv = document.querySelectorAll('div.league-wrapper.sport-S-wrapper.ng-scope');//length = 8
+  fudbalDiv = fudbalDiv[0];
+  var leagueDivs = fudbalDiv.querySelectorAll('p[ui-sref="home.leaguesWithMatches"]');
+  return leagueDivs.length;
 }
 
 //----------------------CASPER-----------------
@@ -166,24 +72,60 @@ casper.then(function() {
   this.click('div.sport-default.sport-S.sports-txt');
 });
 
-casper.wait(3000, function() {
+casper.wait(5000, function() {
 	this.echo("Wait after click...");
 });
 
+casper.then(function () {
+  leaguesCount = this.evaluate(getLeaguesCount) - 1;
+  this.echo("leaguesCount = " + leaguesCount);
+});
+
+casper.then(function () {
+
+  for(var i = 0; i < leaguesCount; i++)
+  {
+    var selector = getNextLeagueDivSelector();
+    this.echo('selector = ' + selector);
+    this.click({ type: 'xpath', path : selector });
+    this.evaluate();
+    currentLeague++;
+  }
+
+  /*var selector = this.evaluate(getNextLeagueDivSelector);
+  this.echo('selector = ' + selector);
+  this.click({
+    type: 'xpath',
+    path : selector
+  });*/
+});
+
+
+casper.wait(2000, function() {
+  this.echo("Wait after click...");
+});
+
+casper.then(function() {
+  //this.capture('test.png');
+});
+
+
+
+/*
 casper.waitForSelector("div.select-all-leagues.select-all-leagues-S", function() {
 	this.capture('img/Fudbal_clicked.png');
 	this.echo("Klik na 'Izaberi sve' filter.");
 	this.click('div.select-all-leagues.select-all-leagues-S');
 });
-
+*/
 //mozda treba zato sto koliko vidim opcija filtera je dostupna i bez tog dugmeta
 /*casper.waitForSelector("button.tip-type-group-button", function() {
 	this.echo("Klik na 'Igre' opciju.");
 	this.click('div.tip-type-group-button');
-});*/
+});
 casper.wait(5000, function() {
 	this.echo("Wait after click...");
-});
+});*/
 
 
 //izgleda sam bio prepusen i nisam video da je 0-2 i 3+ odmah vidljivo
@@ -200,31 +142,11 @@ casper.wait(5000, function() {
 });
 */
 
-
-casper.then(function() {
-	divNumber = this.evaluate(getTableCount);
-	this.echo("divNumber = " + divNumber);
-	this.echo("old = " + oldDivNumber);
-});
-
-casper.then(function () {
-    this.page.scrollPosition = { top: this.page.scrollPosition["top"] + document.body.scrollHeight, left: 0 };
-});
-
-
-casper.wait(5000, function() {
-	this.echo("Wait after scroll...");
-});
-
 /*
 casper.then(function() {
 	casper.scrollToBottom();
 });
 */
-casper.then(function() {
-	this.capture('img/scrolled.png');
-	this.echo("captured scroll");
-});
 
 //casper.waitFor(function() { return oldDivNumber != divNumber }, scrollDown, forseTerminate);
 
